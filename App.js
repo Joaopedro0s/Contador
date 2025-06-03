995c2e1122f7a321b986059941cc6a67e285f1f2
Aluno <jeipi7007@gmail.com>
2025-06-03 16:02:54 -0300

    Commit

--- App.js
+++ App.js
@@ -1,14 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
+import { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  const [loading, setLoading] = useState(true);
+  const [moedas, setMoedas] = useState([]);
+  const [moedaSelecionada, setMoedaSelecionada] = useState(null);
+  const [moedaBValor, setMoedaBValor] = useState("");
+  const [valorMoeda, setValorMoeda] = useState(null);
+  const [valorConvertido, setValorConvertido] = useState(0);
+
+useEffect(() => {
+  async function loadMoedas(){
+    const response = await api.get("all")
+    let arrayMoedas = [];
+    Object.keys(response.data).map( (key) => {
+      arrayMoedas.push({
+        key: key,
+        label: key,
+        value: key,
+      })
+    })
+    *
+    setMoedas(arrayMoedas)
+    setMoedaSelecionada(arrayMoedas[0].key)
+    setLoading(false);
+  }
+  loadMoedas();
+},[]);
+
+
+
+};
 
 const styles = StyleSheet.create({
   container: {
